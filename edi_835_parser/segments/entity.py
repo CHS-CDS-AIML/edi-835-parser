26edb9f93556bab41ba793f896191a52ae1d7a1a
Ray Matsumoto <ray_matsumoto@chs.net>
2024-11-20 23:52:17 +0000

    Set values to none if segment isn't certain length

--- edi_835_parser/segments/entity.py
+++ edi_835_parser/segments/entity.py
@@ -1,37 +1,60 @@
 from edi_835_parser.elements.identifier import Identifier
 from edi_835_parser.elements.entity_code import EntityCode
 from edi_835_parser.elements.entity_type import EntityType
-from edi_835_parser.elements.identification_code_qualifier import IdentificationCodeQualifier
+from edi_835_parser.elements.identification_code_qualifier import (
+    IdentificationCodeQualifier,
+)
 from edi_835_parser.segments.utilities import split_segment, get_element
 
 
 class Entity:
-	identification = 'NM1'
+    identification = "NM1"
 
-	identifier = Identifier()
-	entity = EntityCode()
-	type = EntityType()
-	identification_code_qualifier = IdentificationCodeQualifier()
+    identifier = Identifier()
+    entity = EntityCode()
+    type = EntityType()
+    identification_code_qualifier = IdentificationCodeQualifier()
 
-	def __init__(self, segment: str):
-		self.segment = segment
-		segment = split_segment(segment)
+    def __init__(self, segment: str):
+        self.segment = segment
+        segment = split_segment(segment)
 
-		self.identifier = segment[0]
-		self.entity = segment[1]
-		self.type = segment[2]
-		self.last_name = segment[3]
-		self.first_name = get_element(segment, 4)
-		self.identification_code_qualifier = get_element(segment, 8)
-		self.identification_code = get_element(segment, 9)
+        try:
+            self.identifier = segment[0]
+        except IndexError:
+            self.identifier = None
+        try:
+            self.entity = segment[1]
+        except IndexError:
+            self.entity = None
+        try:
+            self.type = segment[2]
+        except IndexError:
+            self.type = None
+        try:
+            self.last_name = segment[3]
+        except IndexError:
+            self.last_name = None
+        try:
+            self.first_name = get_element(segment, 4)
+        except IndexError:
+            self.first_name = None
+        try:
+            self.identification_code_qualifier = get_element(segment, 8)
+        except IndexError:
+            self.identification_code_qualifier = None
+        try:
+            self.identification_code = get_element(segment, 9)
+        except IndexError:
+            self.identification_code = None
 
-	def __repr__(self):
-		return '\n'.join(str(item) for item in self.__dict__.items())
+    def __repr__(self):
+        return "\n".join(str(item) for item in self.__dict__.items())
 
-	@property
-	def name(self) -> str:
-		return f'{self.first_name} {self.last_name}'.title()
+    @property
+    def name(self) -> str:
+        return f"{self.first_name} {self.last_name}".title()
 
 
-if __name__ == '__main__':
-	pass
+if __name__ == "__main__":
+    pass
