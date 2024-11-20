4a50eff729fb271d4d7996948b9f25ff62dd9454
Ray Matsumoto <ray_matsumoto@chs.net>
2024-11-20 20:18:10 +0000

    Get date

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
-#import requests
 import base64
 import os
+from edi_835_parser.parse_835 import parse_daily_files
 
 app = Flask(__name__)
 
 @app.route("/show_project", methods=['POST', 'GET'])
 def print_project():
     print(f"PROJECT IS {os.getenv('PROJECT')}")
+
+@app.route("/parse_files", methods=['POST'])
+def parse_files():
+    parse_daily_files()
 
 
 if __name__ == "__main__":
