e75b670b6620b1df489c582c04a3ec57f90f9174
Ray Matsumoto <ray_matsumoto@chs.net>
2024-11-22 16:05:19 +0000

    Fix tabs and spaces in diag codes

--- edi_835_parser/edi_837/elements/diagnosis_codes.py
+++ edi_835_parser/edi_837/elements/diagnosis_codes.py
@@ -1,13 +1,15 @@
 from edi_835_parser.elements import Element, Code
+
 # import List
-remark_qualifiers = {'HI': 'diagnosis codes'}
+remark_qualifiers = {"HI": "diagnosis codes"}
+
 
 class RemarkQualifier(Element):
-	def parser(self, value: str) -> List[Code]:
+    def parser(self, value: str) -> List[Code]:
         splits = value.split()
         codes = list()
         for code_str in splits:
             description = code_str.split(":")[0]
-            element = code_str.spit(":")[1]
+            element = code_str.split(":")[1]
             codes.append(Code(element, description))
         return codes
