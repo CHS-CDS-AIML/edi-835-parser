79a291dbd1d906fd02ed98c85c3a0caf0b279e53
Ray Matsumoto <ray_matsumoto@chs.net>
2024-11-22 16:03:47 +0000

    Updated diagnosis codes

---
+++ edi_835_parser/edi_837/elements/diagnosis_codes.py
@@ -0,0 +1,13 @@
+from edi_835_parser.elements import Element, Code
+# import List
+remark_qualifiers = {'HI': 'diagnosis codes'}
+
+class RemarkQualifier(Element):
+	def parser(self, value: str) -> List[Code]:
+        splits = value.split()
+        codes = list()
+        for code_str in splits:
+            description = code_str.split(":")[0]
+            element = code_str.spit(":")[1]
+            codes.append(Code(element, description))
+        return codes
