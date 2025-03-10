7e2f55c3e20ba9cdd24c4bafd08e50fb37f156f0
Ray Matsumoto <ray_matsumoto@chs.net>
2025-03-10 16:16:03 +0000

    Update type of care codes

--- edi_835_parser/elements/claim_facility.py
+++ edi_835_parser/elements/claim_facility.py
@@ -3,23 +3,24 @@ from edi_835_parser.elements import Element, Code
 type_of_facility = {
     1: "hospital",
     2: "skilled nursing facility",
-    3: "home health agency",
+    3: "home health",
     4: "religious nonmedical health care institutions",
+    6: "intermediate care",
     7: "clinic",
     8: "special facility",
+    9: "reserved for national assignemnt",
 }
 
 bill_classification = {
-    1: "inpatient",
-    2: "outpatient",
-    3: "swing bed",
+    1: "inpatient (part A)",
+    2: "inpatient (part B)",
+    3: "outpatient",
     4: "other",
     5: "intermediate care level 1",
     6: "intermediate care level 2",
     7: "subacute inpatient",
-    8: "hospice",
+    8: "NA",
 }
-
 
 class ClaimFacility:
     def parser(self, value: str) -> Code:
